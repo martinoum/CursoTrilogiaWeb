1d2fe06e2b3bb95c724f325a7033858b069adc64
martinoum <martinouvillamorales@gmail.com>
2025-05-15 10:34:08 -0300

    update ej 4 eventos

--- clase3/script.js
+++
@@ -1,8 +0,0 @@
-
-function cambiar() {
-    const encabezado = document.getElementById("encabezado");
-    const parrafo = document.getElementById("parrafo");
-
-    encabezado.innerHTML = "Este es un nuevo titulo con DOM en JavaScript";
-    parrafo.style.backgroundColor = "blue"
-}
